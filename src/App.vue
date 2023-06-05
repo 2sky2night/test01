5ea7763b7ea17d4edf1e036822bda56636d81d46
Kinght <1301680879@qq.com>
2023-06-05 11:12:47 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Minput from '@/components/input/index.vue'
+
+const name = ref('')
+
+</script>
+
+<template>
+  <div>
+    <Minput v-model="name" />
+    {{ name }}
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/input/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="m-input-container">
+    <input :placeholder="placeholder" class="m-input" :value="modelValue" @input="handleChange" />
+  </div>
+</template>
+
+<script lang='ts' setup generic="T">
+
+const props = withDefaults(defineProps<{
+  modelValue: T;
+  placeholder?: string;
+}>(), {
+  placeholder:'请输入内容'
+})
+const emit = defineEmits<{
+  'update:modelValue':[value:T]
+}>()
+
+function handleChange (e:Event) {
+  emit('update:modelValue',(e.target as HTMLInputElement).value)
+}
+
+
+defineOptions({
+  name:'m-input'
+})
+</script>
+
+<style scoped lang='scss'>
+.m-input-container{
+  display: inline-block;
+  .m-input{
+    outline: none;
+    padding: 5px 10px;
+    border: 1px solid #eee;
+    background-color: #eee;
+    transition: .3s;
+    &:focus{
+      background-color: #fff;
+      border: 1px solid rgb(80, 176, 214); 
+    }
+  }
+
+}
+</style>
